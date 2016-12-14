49272a91183770016d39fba669235d881b6a87d1
Collin Krawll <collin.krawll@thirdset.com>
2016-12-13 23:13:57 -0800

    Updated the version number to 1.0.1

--- Config/config.php
+++ Config/config.php
@@ -15,7 +15,7 @@
 return array(
     'name'        => 'Timing',
     'description' => 'Allows for processing actions only during certain times.',
-    'version'     => '1.0',
+    'version'     => '1.0.1',
     'author'      => 'Third Set Productions',
     'services'    => array(
         //MODELS
