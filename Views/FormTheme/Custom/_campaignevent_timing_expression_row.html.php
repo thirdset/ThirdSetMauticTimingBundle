4f31a75ef208bee9c25062a6b7790e4313e1f67f
Collin Krawll <collin.krawll@thirdset.com>
2016-12-14 22:28:52 -0800

    Added a 'Use Work Hours' link net to the Expression field.

---
+++ Views/FormTheme/Custom/_campaignevent_timing_expression_row.html.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     ThirdSetMauticTimingBundle
+ * @copyright   2016 Third Set Productions. All rights reserved.
+ * @author      Third Set Productions
+ * @link        http://www.thirdset.com
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * This template was copied from CoreBundle\Views\FormTheme\Custom\form_row.html.php
+ * It modifies the row for the timing fields and adds an <hr/> tag above it.
+ * 
+ * The folder of theme files is registered in the ThirdSetMauticTimingBundle
+ * class.
+ * 
+ * You can see what other theme files are searched for by editing
+ * Symfony\Bundle\FrameworkBundle\Templating\Loader\FileSystemLoader->load()
+ *   Add the following line at the top of the function:
+ *       echo $template->getPath();
+ *   Then load the form in a browser. Note that you may need to find the output 
+ * by using the browser's Developer Tools.
+ */
+
+$hasErrors     = count($form->vars['errors']);
+$feedbackClass = (!empty($hasErrors)) ? ' has-error' : '';
+?>
+<hr/>
+<div class="row">
+    <div class="form-group col-xs-12 <?php echo $feedbackClass; ?>">
+        <?php echo $view['form']->label($form, $label) ?>
+        <?php echo $view['form']->widget($form) ?>
+        <a href="#" style="margin-left: 1em;" onclick="mQuery('#campaignevent_timing_expression').val('* 09-17 * * 1-5'); return true;" title="Set the expression to 9AM to 5PM, Monday through Friday."><small>Use Work Hours</small></a>
+        <?php echo $view['form']->errors($form) ?>
+    </div>
+</div>
