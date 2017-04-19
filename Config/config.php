59deb3e6017a64c8cbc651f3156ce7d4bcb3cbf4
Collin Krawll <collin.krawll@thirdset.com>
2017-04-18 23:21:51 -0700

    Updated the version number to 1.1.2

--- Config/config.php
+++ Config/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     ThirdSetMauticTimingBundle
- * @copyright   2016 Third Set Productions. All rights reserved.
+ * @copyright   2017 Third Set Productions. All rights reserved.
  * @author      Third Set Productions
  * @link        http://www.thirdset.com
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -15,7 +15,7 @@
 return array(
     'name'        => 'Timing',
     'description' => 'Allows for processing actions only during certain times.',
-    'version'     => '1.1.1',
+    'version'     => '1.1.2',
     'author'      => 'Third Set Productions',
     'services'    => array(
         //MODELS
